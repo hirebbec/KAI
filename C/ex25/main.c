8ad80b93211b32e6d4402f809ab0edc2b24ef389
hirebbec <reek867@gmail.com>
2022-11-19 16:48:04 +0300

    fix

--- C/ex25/main.c
+++ C/ex25/main.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int main() {
-    int m[4][4];
-
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            scanf("%d", &m[i][j]);
+    int size;
+    int *m;
+    scanf("%d", &size);
+    m = malloc(size * sizeof (int));
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &m[i]);
+    }
+    int min = m[0];
+    for (int i = 0; i < size; i++) {
+        if (min > m[i]) {
+            min = m[i];
         }
     }
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            if (j == 3) {
-                printf("%d", m[j][i]);
-            } else {
-                printf("%d ", m[j][i]);
-            }
-        }
-        printf("\n");
-    }
+    printf("%d", min);
 }
